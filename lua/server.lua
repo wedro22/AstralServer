854285007942142f48029fc4686407a43943be25
wedro <wedro2208@gmail.com>
2025-05-09 13:33:48 +0300

    raw script? delete

--- lua/server.lua
+++ lua/server.lua
@@ -1,6 +1,8 @@
 local internet = require("internet")
---local os = require("os")проверка по времени
-local URL="https://wedro22.share.zrok.io/astral"
+--local os = require("os")проверка по времени реализовать потом
+local URL="https://wedro.share.zrok.io/astral/gt/gt"
+local URLGET="https://wedro.share.zrok.io/astral/gt/gt/get/raw"
+local URLPOST="https://wedro.share.zrok.io/astral/gt/gt/post/raw"
 local handle, reason = internet.request(URL)
 if not handle then
     print("Ошибка подключения:", reason)
