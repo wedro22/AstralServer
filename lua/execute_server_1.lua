cf2101e5292fbf02b9a6e23ca58c3cb281e8e161
wedro22 <wedro2208@gmail.com>
2025-05-27 21:19:07 +0300

    test

--- lua/execute_server_1.lua
+++ lua/execute_server_1.lua
@@ -4,7 +4,7 @@ local os = require("os")
 -- Конфигурация
 local CONFIG = {
     GET_URL = "https://wedro22.pythonanywhere.com/astral/gt/gt/get/raw",
-    POST_URL = "https://wedro22.pythonanywhere.com/astral/gt/gt/get/raw",
+    POST_URL = "https://wedro22.pythonanywhere.com/astral/gt/gt/post/raw",
     POLL_INTERVAL = 5, -- секунд
     MAX_OUTPUT_LENGTH = 1024 * 1024 -- максимальная длина вывода (1MB)
 }
--- lua/server2.lua
+++ lua/server2.lua
@@ -5,7 +5,7 @@ local computer = require("computer")
 -- Конфигурация
 local CONFIG = {
     GET_URL = "https://wedro22.pythonanywhere.com/astral/gt/gt/get/raw",
-    POST_URL = "https://wedro22.pythonanywhere.com/astral/gt/gt/get/raw",
+    POST_URL = "https://wedro22.pythonanywhere.com/astral/gt/gt/post/raw",
     POLL_INTERVAL = 5, -- секунд
     MAX_OUTPUT_LENGTH = 1024 * 1024, -- максимальная длина вывода (1MB)
     MEMORY_THRESHOLD = 0.2 -- минимальная доля свободной памяти (20%)
