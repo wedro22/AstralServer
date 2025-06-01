79d8ccac6fd677da09c37f1d8b8a1819677ca376
wedro22 <wedro2208@gmail.com>
2025-06-01 22:36:40 +0300

    longpoll2 ?

--- lua/execute_server_5.lua
+++ lua/execute_server_5.lua
@@ -22,17 +22,17 @@ while true do
     print("Server running at " .. time)
 
     -- Получаем код для выполнения
-    local result, headers, err = longPoll.request(CONFIG.GET_URL2)
+    local result, headers, err = longPoll.request(CONFIG.GET_URL)
     if not err then
         print("Executing code...")
         local p,r,e = executor.safeExecute(result)
         local h = ""
         for i,v in pairs(headers) do
         for ii,vv in pairs(v) do
-            h=h..tostring(i).." "..tostring(ii).." "..tostring(vv).." "
+            h=h..tostring(i).." "..tostring(ii).." "..tostring(vv).."\n"
         end
     end
-        longPoll.request(CONFIG.POST_URL, "[P] Prints:\n"..p.."\n[R] Returns:\n"..r.."\n[E] Errors:\n"..e.."\n[H] Headers:\n"..)
+        longPoll.request(CONFIG.POST_URL, "[P] Prints:\n"..p.."\n[R] Returns:\n"..r.."\n[E] Errors:\n"..e.."\n[H] Headers:\n"..h)
         print("Execution completed, result sent")
     else
         print(err)
